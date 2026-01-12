9a275bed23e151014b2d4ddd23ae5d1824f720ef
Giovana <gnyokota@gmail.com>
2026-01-12 18:08:36 +0100

    chore: first python inputs

---
+++ app.py
@@ -0,0 +1,13 @@
+# log: python hello.py
+age=25
+print("Hello World!") 
+print(age)            
+first_name="John"
+is_online=True 
+
+name= input("Enter your name: ")
+print("Hello " + name)
+
+birth_year= input("Enter your birth year: ")
+age= 2026 - int(birth_year)
+print("Your age is: " + str(age))
