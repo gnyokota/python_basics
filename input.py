741c0ab0b59eb07c4b26e509e3678c00856c1ea5
Giovana <gnyokota@gmail.com>
2026-01-12 21:10:59 +0100

    chore: add input file

---
+++ input.py
@@ -0,0 +1,9 @@
+first = input("Enter your first value: ")
+second = input("Enter your second value: ")
+sum = float(first) + float(second)
+print("The sum is: " + str(sum))
+
+course = 'Python Programming'
+print(course.upper())
+print(course.find('Pro'))
+print('python' in course.lower())
