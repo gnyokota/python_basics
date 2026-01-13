6e359401d62812ccda6cde3bfa6274d57cc678aa
Giovana <gnyokota@gmail.com>
2026-01-13 21:09:58 +0100

    chore: add tuples

--- list.py
+++ list.py
@@ -13,3 +13,13 @@ print("After inserting 5 at the beginning:", numbers)
 numbers.remove(30)
 print("After removing 30:", numbers)
 print(10 in numbers)
+
+ranges=list(range(1, 11))
+print("Numbers from 1 to 10:", ranges)
+new_ranges=range(5)
+print("Numbers from 0 to 4:", list(new_ranges))
+
+immutable_tuple=(1, 2, 3)
+# immutable_tuple[0] = 10  # This will raise a TypeError
+print("Tuple contents:", immutable_tuple)
+# imutable_tuple.append(4)  # This will raise an AttributeError
