793ea4e72fe9cddfa978679fe7d176849127ba29
Giovana <gnyokota@gmail.com>
2026-01-13 20:27:04 +0100

    chore: add if block

--- arithmetic.py
+++ arithmetic.py
@@ -4,4 +4,17 @@ x+=2
 print(x)
 y=10>12
 print(y)
-//29
+z=10==10
+print(z)
+a=15!=15   
+print(a)
+price=5
+print(price>10 or price<30)
+
+temperature=9
+if temperature>30:
+    print("It's a hot day")
+elif temperature<10:
+    print("It's a cold day")
+else:
+    print("It's a lovely day")
